167c5ff9f59c5c362c898bcfa0a439a0b92dbbf9
Joons <elddy0948@gmail.com>
2021-03-15 03:25:12 +0900

    a description of the chages I made

--- ProjectManagerServer/Sources/App/routes.swift
+++ ProjectManagerServer/Sources/App/routes.swift
@@ -1,6 +1,7 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    app.http.server.configuration.hostname = "example.herokuapp.com"
     app.get { req in
         return "It works!"
     }
